86c48925bef749dfbe4b1351cfeaee54810168da
Krzysztof Streflik <streflik@gmail.com>
2012-08-10 17:58:08 +0200

    version changed

--- lib/ajax-datatables-rails.rb
+++ lib/ajax-datatables-rails.rb
@@ -4,7 +4,7 @@ class AjaxDatatablesRails
   
   class MethodError < StandardError; end
 
-  VERSION = '0.0.1'
+  VERSION = '0.0.2'
     
   attr_reader :columns, :model_name, :searchable_columns, :filters_parameter
 
