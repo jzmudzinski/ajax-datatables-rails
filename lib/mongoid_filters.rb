dc3cb05e38b42ab346b6b31fd5a1aacb354ca6b4
Krzysztof Streflik <streflik@gmail.com>
2012-10-25 15:46:57 +0200

    added new filters

--- lib/mongoid_filters.rb
+++ lib/mongoid_filters.rb
@@ -7,18 +7,27 @@ module Mongoid
       def filter(attrs)
 
         attrs.each do |method|
-          eval "attr_accessor :#{method}_gte, :#{method}_lte, :#{method}_eq"
+          eval "attr_accessor :#{method}_gte, :#{method}_lte, :#{method}_eq, :#{method}_is_null, :#{method}_in_not_null"
         end
 
         def filter_criteria(filters = nil)
             f = {}
             if filters.present?
               filters.each_pair do |k,v|
-                match = k.match(/\b(\w*)_(gte|lte|eq)\b/)
+                match = k.match(/\b(\w*)_(gte|lte|eq|is_null|is_not_null)\b/)
                 if match[2] == "eq"
                   f.merge!({"#{match[1]}" => v})
+                elsif match[2] == "is_null"
+                  f.merge!({"#{match[1]}" => nil })
+                elsif match[2] == "is_not_null"
+                  f.merge!({"#{match[1]}" => {"$ne" => nil} })
                 else
-                  f.merge!({"#{match[1]}" => {"$#{match[2]}" => v}})
+                  f.each_pair{|k,v| found_key = k if k.to_s == match[1] && v.class.name == "Hash"}
+                  if found_key
+                    f[found_key].merge!({"$#{match[2]}" => v})
+                  else
+                    f.merge!({"#{match[1]}" => {"$#{match[2]}" => v}})
+                  end
                 end
               end
             end
